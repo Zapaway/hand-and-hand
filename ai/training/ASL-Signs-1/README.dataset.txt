290351ea8cfd7468f600ca74066f92acc7b989de
Your Name <tiahi.m@northeastern.edu>
2024-09-14 21:59:33 -0400

    Help me

---
+++ ai/training/ASL-Signs-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# ASL Signs > 2024-09-15 1:24am
+https://universe.roboflow.com/mit-itdvv/asl-signs
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- ai/training/yolo.py
+++ ai/training/yolo.py
@@ -6,6 +6,7 @@ model = YOLO("yolov8n.pt")  # pretrained YOLOv8n model
 # Run batched inference on a list of images
 results = model(["hands.jpg"])  # return a list of Results objects
 
+
 # Process results list
 for result in results:
     boxes = result.boxes  # Boxes object for bounding box outputs
